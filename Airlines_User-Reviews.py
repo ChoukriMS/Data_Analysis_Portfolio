dfb8031eff1d573a221142ebfbeff76fd4f1d321
Mohamed Choukri Soukehal <Mohamed-Choukri.Soukehal@dgfc.ulaval.ca>
2023-11-17 11:38:11 -0500

    Data Cleaning first iteration done

--- Airlines_User-Reviews.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-reviews = pd.read_csv("Airlines_User-Reviews_Raw.csv")
-reviews.info()
-
-##
-print(reviews.count()-reviews.drop_duplicates().count())
-#Duplicate are not usefull in our case 
-reviews = reviews.drop_duplicates()
